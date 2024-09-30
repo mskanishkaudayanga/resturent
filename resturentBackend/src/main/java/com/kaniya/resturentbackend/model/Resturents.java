8f399a313d16b44f9d6e01d7f2e283646ca46e6f
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-09-30 17:55:51 +0530

    create resturent

--- resturentBackend/src/main/java/com/kaniya/resturentbackend/model/Resturents.java
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/model/Resturents.java
@@ -1,8 +1,23 @@
 package com.kaniya.resturentbackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Resturents {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String name;
+    private String ResturentName;
     private String Email;
     private String Password;
     private String phoneNumber;
@@ -13,5 +28,14 @@ public class Resturents {
     private String CloseTime;
     private String openingDays;
 
-    private Menu menu;
+    @OneToMany(mappedBy = "resturents" ,cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Menu> menu;
+
+    public Resturents(String resturentName, String email, String password, String phoneNumber) {
+        this.ResturentName = resturentName;
+        this.Email = email;
+        this.Password = password;
+        this.phoneNumber = phoneNumber;
+
+    }
 }
