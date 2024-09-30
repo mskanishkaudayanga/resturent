5a496de955c856976e744c5862f0e66ba59f0dcd
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-09-30 17:55:39 +0530

    create dish

--- resturentBackend/src/main/java/com/kaniya/resturentbackend/model/Dishes.java
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/model/Dishes.java
@@ -1,4 +1,26 @@
 package com.kaniya.resturentbackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Dishes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  long id;
+    private  String dishName;
+    private  Double dishPrice;
+    private  String dishDescription;
+    private String dishImage;
+
+    @ManyToOne
+    @JoinColumn(name="Category_is")
+    private Category category;
 }
