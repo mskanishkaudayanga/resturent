2cccd1f768b9af1ba939b75c2b66ed69bdf72e4f
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-10-01 01:14:48 +0530

    exception

---
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/exceptions/MenuNotFoundExeption.java
@@ -0,0 +1,8 @@
+package com.kaniya.resturentbackend.exceptions;
+
+public class MenuNotFoundExeption extends RuntimeException {
+    public MenuNotFoundExeption(String message) {
+
+        super(message);
+    }
+}
