b38fb1d16f6ba1ae5bc9e233890bc3f19ccab61f
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-09-30 17:57:25 +0530

    exception not found restirent

---
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/exceptions/ResturentNotFoundExeption.java
@@ -0,0 +1,7 @@
+package com.kaniya.resturentbackend.exceptions;
+
+public class ResturentNotFoundExeption extends RuntimeException {
+    public ResturentNotFoundExeption(String message) {
+        super(message);
+    }
+}
