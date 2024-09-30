9086eaac4d8f03f54b5a866f739b053d4cdefdc1
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-09-30 17:56:01 +0530

    create menu

--- resturentBackend/src/main/java/com/kaniya/resturentbackend/model/Menu.java
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/model/Menu.java
@@ -1,4 +1,31 @@
 package com.kaniya.resturentbackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Menu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String MenuName;
+
+
+    @ManyToOne
+    @JoinColumn(name="Resturent_is")
+    private Resturents resturents;
+
+    @OneToMany(mappedBy = "menu" ,cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> category;
+
+
 }
