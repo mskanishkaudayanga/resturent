96f0aad41a1a5d2d868a3cd78ecd93aabbcf5f6c
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-09-30 17:56:56 +0530

    resturent repository

---
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/repository/ResturentRepository.java
@@ -0,0 +1,8 @@
+package com.kaniya.resturentbackend.repository;
+
+import com.kaniya.resturentbackend.model.Resturents;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResturentRepository extends JpaRepository<Resturents ,Long> {
+
+}
