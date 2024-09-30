10f2f60662c1e6c830f51da0f026a1d20711fffb
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-09-30 17:56:12 +0530

    create category

--- resturentBackend/src/main/java/com/kaniya/resturentbackend/model/Category.java
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/model/Category.java
@@ -1,4 +1,28 @@
 package com.kaniya.resturentbackend.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String CategoryName;
+
+    @ManyToOne
+    @JoinColumn(name="Menu_is")
+    private Menu menu;
+
+    @OneToMany(mappedBy = "category" ,cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Dishes> dishes;
 }
