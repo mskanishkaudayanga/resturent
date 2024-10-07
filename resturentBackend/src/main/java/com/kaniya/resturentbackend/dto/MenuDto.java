50388d07a4f9fa839cf02af784b6635dbec7cd99
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-10-07 22:37:25 +0530

    dto

--- resturentBackend/src/main/java/com/kaniya/resturentbackend/dto/MenuDto.java
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/dto/MenuDto.java
@@ -1,4 +1,15 @@
 package com.kaniya.resturentbackend.dto;
 
+import com.kaniya.resturentbackend.model.Dishes;
+import com.kaniya.resturentbackend.model.Resturents;
+import jakarta.persistence.*;
+
+import java.util.List;
+
 public class MenuDto {
+
+    private long id;
+    private String menuName;
+    private Resturents resturents;
+    private List<Dishes> dishes;
 }
--- resturentBackend/src/main/java/com/kaniya/resturentbackend/dto/ResturentDto.java
+++ resturentBackend/src/main/java/com/kaniya/resturentbackend/dto/ResturentDto.java
@@ -22,3 +22,4 @@ public class ResturentDto {
     private List<MenuDto> menu;
 
 }
+ 
